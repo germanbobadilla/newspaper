e47ccaf4626426d30e25e4659314cb40d8763bf0
German Bobadilla <germanbobadilla@gmail.com>
2022-10-21 18:14:38 -0400

    Commit Day 3

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 // import { setConcentGallery } from './components/content-gallery/content-gallery.js';
 import { addNavigation } from './components/navigation/navigation.js';
+import { Footer } from './components/footer-component/footer-component.js';
+const btn = document.getElementById('showFooter');
 addNavigation();
+function showFooter() {
+  return customElements.define('footer-component', Footer);
+}
+
+function removeFooter() {
+  return customElements.define('footer-component', Footer);
+}
+
+btn.addEventListener('click', (event) => {
+  showFooter();
+});
+
+// Footer();
+
+localStorage.setItem('myCat', 'Tom');
+const cat = localStorage.getItem('myCat');
+
+console.log(cat);

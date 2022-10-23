2fb7fe8873ec5bfce6299b2257101d87d89e0e80
German Bobadilla <germanbobadilla@gmail.com>
2022-10-22 21:49:49 -0400

    Day 4

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,20 +1,12 @@
+import routes from './routes.js';
 export const addNavigation = () => {
-  const navLinks = `
-        <div class="navigation">
-          <button class="sections">Sections</button>
-          <ul>  
-            <li class="navLinks">Software Engineering</li>
-            <li class="navLinks">Computer Science</li>
-            <li class="navLinks">Github</li>
-            <li class="navLinks">FAANG</li>
-            <li class="navLinks">Reddit</li>
-            <li class="navLinks">Books</li>
-            <li class="navLinks">Tutorials</li>
-          </ul>
-        </div>
-     `;
+  const navLinks = routes.map((name, key) => {
+    `<ul ${key}><li><a>${name}<a/></li></ul>`;
+    return navLinks;
+  });
   const nav = document.getElementById('nav');
   const ul = document.createElement('ul');
+  ul.setAttribute('class', 'navLinks');
   nav.appendChild(ul);
   ul.innerHTML = navLinks;
 };
---
+++ components/navigation/routes.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: 'Software Engineering'
+  },
+  {
+    name: 'FAANG'
+  },
+  {
+    name: 'Interview'
+  },
+  {
+    name: 'Computer Science'
+  },
+  {
+    name: 'Books'
+  },
+  {
+    path: 'Tutorials'
+  }
+];

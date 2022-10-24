4a70b1e02b5c04409967e42cb4f11460dd422b42
German Bobadilla <germanbobadilla@gmail.com>
2022-10-23 22:31:26 -0400

    Day 5

--- main.js
+++ main.js
@@ -2,10 +2,36 @@
 import { addNavigation } from './components/navigation/navigation.js';
 addNavigation();
 import { Footer } from './components/footer-component/footer-component.js';
-import { fetchWeatherAPI } from './components/navigation/weather/weather-api.js';
+// import { fetchWeatherAPI } from './components/navigation/weather/weather-api.js';
 
 // Footer();
 customElements.define('footer-component', Footer);
 
 // API Calls
-fetchWeatherAPI();
+// fetchWeatherAPI();
+
+// Designing the slider class
+
+class Slider {
+     constructor(mov, time, state){
+          this.mov = mov;
+          this.time = time;
+          this.state = state;
+     }
+
+     static parentSlides = "Slider";
+     static 
+
+     getSlides() {
+          for (const slide of this.slides){
+               yield slide
+          }
+     }
+}
+
+const parentSlider = document.querySelector('.parent-slider');
+const slides = parentSlider.querySelectorAll('.child-slider');
+
+console.log(frames);
+
+slides.getSlides('left', '200', false)

ab4056e5386449fd3f1678b7a45854b8cb087e2a
German Bobadilla <germanbobadilla@gmail.com>
2022-10-25 19:47:35 -0400

    Day 6

--- main.js
+++ main.js
@@ -10,6 +10,16 @@ customElements.define('footer-component', Footer);
 // Anja
 
 const slidesContainer = document.getElementById('slides-container');
-const slides = document.querySelector('.slide');
-const preButton = document.getElementById('slide-arrow-prev');
+const slide = document.querySelector('.slide');
+const prevButton = document.getElementById('slide-arrow-prev');
 const nextButton = document.getElementById('slide-arrow-next');
+
+nextButton.addEventListener('click', (event) => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener('click', () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});

9a458cc56757f939ae8d41cccf73d7c488794f4b
German Bobadilla <germanbobadilla@gmail.com>
2022-10-27 21:09:31 -0400

    Day 8

--- main.js
+++ main.js
@@ -6,20 +6,3 @@ import { Footer } from './components/footer-component/footer-component.js';
 
 // Footer();
 customElements.define('footer-component', Footer);
-
-// Anja
-
-const slidesContainer = document.getElementById('slides-container');
-const slide = document.querySelector('.slide');
-const prevButton = document.getElementById('slide-arrow-prev');
-const nextButton = document.getElementById('slide-arrow-next');
-
-nextButton.addEventListener('click', (event) => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
-
-prevButton.addEventListener('click', () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});

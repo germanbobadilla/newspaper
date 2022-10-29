7505202acf57aa2816dc65e2c822b854f3ba7501
German Bobadilla <germanbobadilla@gmail.com>
2022-10-29 00:35:21 -0400

    Day 10

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // import { setConcentGallery } from './components/content-gallery/content-gallery.js';
 import { addNavigation } from './components/navigation/navigation.js';
-addNavigation();
 import { Footer } from './components/footer-component/footer-component.js';
+import articles from './components/_mocks_/mock-data.js';
+// console.log(articles.articles[0].title);
+addNavigation();
 // import { fetchWeatherAPI } from './components/navigation/weather/weather-api.js';
 
 // Footer();

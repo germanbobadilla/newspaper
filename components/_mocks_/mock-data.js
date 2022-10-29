c043c918fd44d48f160dce1d3e837e3821f734f1
German Bobadilla <germanbobadilla@gmail.com>
2022-10-29 01:30:13 -0400

    Day 10

--- components/_mocks_/mock-data.js
+++ components/_mocks_/mock-data.js
@@ -1,5 +1,5 @@
-export default {
-  articles: [
+export const articles = {
+  items: [
     {
       source: {
         id: 'reuters',
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // import { setConcentGallery } from './components/content-gallery/content-gallery.js';
 import { addNavigation } from './components/navigation/navigation.js';
 import { Footer } from './components/footer-component/footer-component.js';
-import articles from './components/_mocks_/mock-data.js';
-// console.log(articles.articles[0].title);
+import { articles } from './components/_mocks_/mock-data.js';
+console.log(articles.items[2].title);
 addNavigation();
 // import { fetchWeatherAPI } from './components/navigation/weather/weather-api.js';
 

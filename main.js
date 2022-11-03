ca4111a53c369b1051ad708bf34c99f8c8df4647
German Bobadilla <germanbobadilla@gmail.com>
2022-11-02 23:51:57 -0400

    Day 14

--- main.js
+++ main.js
@@ -44,3 +44,16 @@ customElements.define('footer-component', Footer);
 // };
 
 // document.addEventListener('submit', handleSearch);
+
+function draw() {
+  const canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+  }
+}
+
+draw();

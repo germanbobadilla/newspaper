0f1fabbdf6dd45a6acafb0b22d61fd8bd8985086
German Bobadilla <germanbobadilla@gmail.com>
2022-11-05 23:37:33 -0400

    Comit 15

--- main.js
+++ main.js
@@ -28,32 +28,3 @@ addNavigation();
 
 // Footer();
 customElements.define('footer-component', Footer);
-
-//  Search
-
-// const handleSearch = function (e) {
-//   e.preventDefault();
-//   let term = e.target.elements['search'].value;
-//   let setTerm = term
-//     .toLowerCase()
-//     .split(' ')
-//     .filter(function (token) {
-//       return token.trim() !== '';
-//     });
-
-// };
-
-// document.addEventListener('submit', handleSearch);
-
-function draw() {
-  const canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
-
-    ctx.fillRect(25, 25, 100, 100);
-    ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
-  }
-}
-
-draw();

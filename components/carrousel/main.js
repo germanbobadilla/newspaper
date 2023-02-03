57b6e54263deca35d9f36c351a303641c966eb27
German Bobadilla <germanbobadilla@gmail.com>
2023-02-03 15:26:36 -0400

    designing the carousel

--- components/carrousel/main.js
+++ components/carrousel/main.js
@@ -1,3 +1,17 @@
 // export const setConcentGallery = (content) => {
 //   let tab = document.getElementById('tab')
 // };
+const slidesContainer = document.getElementById('slides-container');
+const slide = document.querySelector('.slide');
+const prevButton = document.getElementById('slide-arrow-prev');
+const nextButton = document.getElementById('slide-arrow-next');
+
+nextButton.addEventListener('click', (event) => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener('click', () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});

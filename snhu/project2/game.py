0fd69255a75dcb0f2ff1ad0516dd867993e863d2
German Bobadilla <germanbobadilla@gmail.com>
2023-02-19 15:45:27 -0400

    finishing the testBasedGame

--- snhu/project2/game.py
+++ snhu/project2/game.py
@@ -1,141 +1,100 @@
 # German Bobadilla
 
+# Let's import the random library
 import random
 
-countries = [
-    ["USA", True, "North"],
-    ["Mexico", True, "West"],
-    ["Canada", True, "South"],
-    ["Brazil", True, "East"],
-    ["Argentina", True, "West"],
-    ["Peru", True, "South"],
-    ["Colombia", True, "North"],
-    ["Venezuela", True, "East"],
-]
-
-# Every diamond that is picked sums to 1
-score = 0
-
-
-def diamond_collector():  # Here we create a function for the villain that lands
-    return random.choice(countries)
-
-
-def ask_door(country):  # This function helps the player navigate.
-    while True:
-        door = input(
-            f"Which door would you like to enter {country[0]} through (North, South, East, West)? ")
-        if door in country:
-            return door
-        print(
-            f"{door} doesn't give you access to {country[0]}. Please enter a valid door.")
-
-
-def play_game():
-    global score
-    current_country = random.choice(countries)
-    print(f"You are now in {current_country[0]}")
-
-    villain = diamond_collector()
-
-    while countries:
-        door = ask_door(current_country)
-
-        if door == current_country[2]:
-            if current_country[1]:
-                pick = input(
-                    f"There is a diamond in {current_country[0]}. Would you like to pick it up? (yes/no) ")
-                if pick == "yes":
-                    score += 1
-                    print(
-                        f"You picked up a diamond in {current_country[0]}. Your score is now {score}.")
-                current_country[1] = False
-            else:
-                print("There is no diamond in this country.")
-            countries.remove(current_country)
-            if not countries:
-                print(
-                    "Congratulations! You've explored all the countries and picked up all the diamonds.")
-                break
-            current_country = random.choice(countries)
-            print(f"You are now in {current_country[0]}")
-
-        if current_country == villain:
-            print("Game over. The villain caught you.")
-            break
-
-
-play_game()
-
-
-# With Main function
-
-# Define a dictionary of rooms and their connections
+# Here, we create the rooms
 rooms = {
-    'Great Hall': {'South': 'Bedroom', 'North': 'Dungeon', 'East': 'Kitchen', 'West': 'Library'},
-    'Bedroom': {'North': 'Great Hall', 'East': 'Cellar', 'item': 'Armor'},
-    'Cellar': {'West': 'Bedroom', 'item': 'Helmet'},
-    'Dining Room': {'South': 'Kitchen', 'item': 'Dragon'}  # villain
+    'Porch': {'North': 'Great Hall'},
+    'Great Hall': {'South': 'Bedroom'},
+    'Bedroom': {'North': 'Great Hall', 'East': 'Cellar'},
+    'Cellar': {'West': 'Bedroom', 'East': 'Kitchen'},
+    'Kitchen': {'North': 'Cellar', 'South': 'Bathroom'},
+    'Main bedroom': {'South': 'Great Hall', 'West': 'Jacuzzi'},
+    'Bathroom': {'East': 'Main bedroom', 'West': 'Kitchen'},
+    'Jacuzzi': {'South': 'Porch'}
 }
 
-# Initialize the player's starting location, inventory, and win condition
-location = 'Great Hall'
-inventory = []
-win = False
+# Initialize the global variable to count the diamonds picked.
+score = 0
+
+# For each room, we want to have a diamond (gem) inside
+diamonds = {room: True for room in rooms}
+
+# This function calls the villain
+# I wanted to call it (Diamond Collector)
 
 
-def show_commands():
-    """Display the list of available commands to the player"""
-    print("Available commands:")
-    print("- go [North/East/South/West]")
-    print("- get [item name]")
+def diamond_collector(rooms):
+    # randomly choose a room for the villain to land on
+    villain_position = random.choice(list(rooms.keys()))
+    print(
+        f"Oh no! The diamond collector is on the loose and has landed on {villain_position}!")
+    # This variable is used inside the main function
+    return villain_position
+
+# This function calls the player's picking up each diamond every time lands on a room
 
 
-def show_status():
-    """Display the player's current status"""
-    print("You are in:", location)
-    print("Inventory:", inventory)
-    print("Items in the room:", rooms[location])
+def diamond_picker(room):
+    global score
+    # check if the room still has a diamond to pick
+    if not diamonds[room]:
+        return
+
+    # prompt the player to pick the diamond
+    answer = input(
+        f"There's a diamond in the {room}. Do you want to pick it? (yes/no)").lower()
+    if answer == 'yes':
+        # Once yes is selected, add 1 to score
+        print("You picked the diamond in the", room)
+        score = score + 1
+        print(f"Your score is now {score}")
+        # Remove the diamond in the room
+        diamonds[room] = False
 
 
-def main():
-    global location, inventory, win
-    # Show the instructions and the player's starting status
-    show_instructions()
-    show_status()
+def main(rooms):
+    global score
+    # Set the initial position of the player to the Great Hall.
+    current_room = 'Great Hall'
+    # Continuously prompt the player for direction until they choose to exit
+    while True:
+        # check if the player has picked all the diamonds
+        if score == 8:
+            print("Congratulations! You have collected all diamonds and defeated the villain!\nThanks for playing the game. Hope you enjoyed it.")
+            break
 
-    # Main game loop
-    while not win:
-        # Get the player's command
-        command = input("Enter your command: ")
-        # Parse the player's command
-        if command.startswith("go"):
-            # Extract the direction from the command
-            direction = command.split()[1]
-            # Check if the direction is valid
-            if direction in rooms[location]:
-                location = rooms[location][direction]
-                # Check if the player has picked up the item in the new room
-                if 'item' in rooms[location]:
-                    item = rooms[location]['item']
-                    inventory.append(item)
-                    print("You picked up a", item)
-                    del rooms[location]['item']
-            else:
-                print("You can't go that way.")
-        elif command == "get Dragon":
-            # Check if the player has found the villain
-            if location == 'Dining Room':
-                win = True
-                print("You have defeated the dragon and won the game!")
-            else:
-                print("The dragon is not here.")
+        # Tell the player in which room s/he is currently in then ask where to go.
+        print("You are in the", current_room)
+        diamond_picker(current_room)
+        print("From here you can go:")
+        # Loop through the available doors in the current room
+        for door in rooms[current_room]:
+            print("\t", door)
+
+        # Ask the player for the direction they want to move
+        # Capitalize the input from the user to the variable.
+        direction = input("Which direction do you want to go? ").capitalize()
+
+        # Check if the direction is valid and move the player to the new room if it is
+        if direction in rooms[current_room]:
+            current_room = rooms[current_room][direction]
+            # check if the player has landed on the same room as the villain
+            if current_room == villain_position:
+                print(
+                    f"Oh no! The diamond collector caught you in {current_room}!")
+                print(
+                    "NOM NOM...GAME OVER! Thanks for playing the game. Hope you enjoyed it.")
+                break
+        # Check if the user inputs "Exit" or "Quit" to break the game
+        elif direction == "Exit" or direction == "Quit":
+            break
         else:
-            print("Invalid command.")
-        # Show the player's updated status
-        show_status()
+            # Inform the player if they entered an invalid direction
+            print("Invalid direction. Please try again.")
 
 
 # Call the main function to start the game
-if __name__ == "__main__":
-    main()
+villain_position = diamond_collector(rooms)
+main(rooms)
